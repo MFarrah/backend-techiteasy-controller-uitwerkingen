4483d7c1d0bbe33710fec9f7152d3b6facbc0aa0
Michael Farrah Girgis <micfar85@gmail.com>
2024-02-02 14:18:50 +0100

    Springboot project started

---
+++ src/main/java/nl/mfarr/backendcontrolleruitwerkingen/BackendControllerUitwerkingenApplication.java
@@ -0,0 +1,13 @@
+package nl.mfarr.backendcontrolleruitwerkingen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendControllerUitwerkingenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendControllerUitwerkingenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/mfarr/backendcontrolleruitwerkingen/BackendControllerUitwerkingenApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mfarr.backendcontrolleruitwerkingen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendControllerUitwerkingenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
