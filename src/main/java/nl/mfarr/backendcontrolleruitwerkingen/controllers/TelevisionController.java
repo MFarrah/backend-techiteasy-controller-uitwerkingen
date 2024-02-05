1a97dc9a3c164e40eb7b3733bb27fe34b3f8c0d6
Michael Farrah Girgis <micfar85@gmail.com>
2024-02-05 19:33:50 +0100

    @Putmapping and @Deletemapping added

--- src/main/java/nl/mfarr/backendcontrolleruitwerkingen/controllers/TelevisionController.java
+++ src/main/java/nl/mfarr/backendcontrolleruitwerkingen/controllers/TelevisionController.java
@@ -22,13 +22,13 @@ public class TelevisionController {
     }
     @GetMapping("/{id}")
     public ResponseEntity<Television> getTelevision(@PathVariable Long id){
-        var optionalTelevision = findCarById(id);
+        var optionalTelevision = findTelevisionById(id);
         if (optionalTelevision==null) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(optionalTelevision);
     }
-    private Television findCarById(Long id) {
+    private Television findTelevisionById(Long id) {
         for (Television television : televisionList) {
             if (television.getId().equals(id)) {
                 return television;
@@ -50,6 +50,34 @@ public class TelevisionController {
     private List<Television> getAllTelevisionsFromList() {
         return televisionList;
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Television> updateTelevision(@PathVariable Long id, @RequestBody Television television) {
+        var existingTelevision = findTelevisionById(id);
+        if (existingTelevision==null) {
+            return ResponseEntity.notFound().build();
+        }
+        existingTelevision.setType(television.getType());
+        existingTelevision.setName(television.getName());
+        existingTelevision.setBrand(television.getBrand());
+        existingTelevision.setPrice(television.getPrice());
+        existingTelevision.setOriginalStock(television.getOriginalStock());
+        existingTelevision.setSold(television.getSold());
+        //geen idee hoe ik de Options lijst update feature zou moeten schrijven. Want deze bevat een lijst van objecten.//
+        return ResponseEntity.ok(existingTelevision);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Television> deleteTelevision(@PathVariable Long id) {
+        var existingTelevision = findTelevisionById(id);
+        if (existingTelevision==null) {
+            return ResponseEntity.notFound().build();
+        }
+        televisionList.remove(existingTelevision);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 
 }
