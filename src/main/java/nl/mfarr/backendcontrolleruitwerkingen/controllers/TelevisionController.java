ce0e2bad1b24bf22b8e6412de1a0d0bc0d436e60
Michael Farrah Girgis <micfar85@gmail.com>
2024-02-05 15:18:39 +0100

    @Getmapping created  for all televisions and per ID

--- src/main/java/nl/mfarr/backendcontrolleruitwerkingen/controllers/TelevisionController.java
+++ src/main/java/nl/mfarr/backendcontrolleruitwerkingen/controllers/TelevisionController.java
@@ -2,10 +2,7 @@ package nl.mfarr.backendcontrolleruitwerkingen.controllers;
 import nl.mfarr.backendcontrolleruitwerkingen.model.Television;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,4 +20,36 @@ public class TelevisionController {
         televisionList.add(television);
         return ResponseEntity.status(HttpStatus.CREATED).body(television);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Television> getTelevision(@PathVariable Long id){
+        var optionalTelevision = findCarById(id);
+        if (optionalTelevision==null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optionalTelevision);
+    }
+    private Television findCarById(Long id) {
+        for (Television television : televisionList) {
+            if (television.getId().equals(id)) {
+                return television;
+            }
+        }
+        return null;
+    }
+    @GetMapping
+    public ResponseEntity<List<Television>> getAllTelevisions() {
+        List<Television> allTelevisions = getAllTelevisionsFromList();
+
+        if (allTelevisions.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(allTelevisions);
+    }
+
+    private List<Television> getAllTelevisionsFromList() {
+        return televisionList;
+    }
+
+
 }
